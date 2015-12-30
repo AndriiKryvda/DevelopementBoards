ee45a345d77266a146d1df98744ed7efbabff1ec
Andrii Kryvda <andrii_kryvda@epam.com>
2015-12-31 00:37:51 +0200

    Integration with DHT11 sensor

--- NodeMCU/test_temperature_01.lua
+++ NodeMCU/test_temperature_01.lua
@@ -1,26 +1,59 @@
+-- init constants
+pin = 5
+wifi_name = "ank"
+wifi_pwd = "TanyaIraAndrey"
+temperature_high = 26
+temperature_low = 24
+
 -- init global variables
+cooler_is_active = false;
 m_temperature = {}
+m_humidity = {}
 for i=0, 4 do
-  m_temperature[i] = -273;
+  m_temperature[i] = -273.0;
+  m_humidity[i] = 0;
 end
 
 -- init alarms
-tmr.alarm(2, 1000, 1, function() CheckCurrentTemperature() end )
-tmr.alarm(3, 2000, 1, function() print(GetAverageTemperature()) end )
---tmr.alarm(0, 10000, 1, function() ShowWifiStatus() end )
---tmr.alarm(1, 30000, 1, function() ReconnectWifi() end )
+tmr.alarm(1, 1000, 0, function() ReconnectWifi() end ) -- initial connect to WiFi
+--tmr.alarm(2, 10000, 1, function() ShowWifiStatus() end )
+tmr.alarm(3, 60000, 1, function() ReconnectWifi() end )
+tmr.alarm(4, 1000, 1, function() CheckCurrentTemperature() end )
+tmr.alarm(5, 10000, 1, function() RunCooler() end )
+
+
+-- ****************************************************************
+function RunCooler()
+  local average_temperature = GetAverageTemperature();
+  if average_temperature >= temperature_high and cooler_is_active == false then
+    print("Cooler is ON, temperature "..average_temperature);
+    cooler_is_active = true;
+    return;
+  end
+  if average_temperature <= temperature_low and cooler_is_active == true then
+    print("Cooler is OFF, temperature "..average_temperature);
+    cooler_is_active = false;
+    return;
+  end
+end
 
 
 function CheckCurrentTemperature()
   for i=1, 4 do
     m_temperature[i] = m_temperature[i-1];
+    m_humidity[i] = m_humidity[i-1];
   end
-  m_temperature[0] = GetCurrentTemperature();
+  GetCurrentTemperature();
 end
 
--- TODO
+
 function GetCurrentTemperature()
-  return 25;
+    status,temp,humi,temp_decimial,humi_decimial = dht.read(pin)
+    if( status == dht.OK ) then
+      --print("DHT Temperature:"..temp..";".."Humidity:"..humi)
+      m_temperature[0] = temp;
+      m_humidity[0] = humi;
+    end
 end
 
 
@@ -54,7 +87,7 @@ function ReconnectWifi()
   if wifi.sta.getip() == nil then 
     print("Connecting to WiFi...")
     wifi.setmode(wifi.STATION)
-    wifi.sta.config ( "ank3_samsung" , "t0937031624" ) 
+    wifi.sta.config (wifi_name, wifi_pwd ) 
     wifi.sta.connect()
   end
 end
